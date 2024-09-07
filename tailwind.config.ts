1c8ed4eab076c9fa53c83fd572f4bcfe16587b5c
Ali <eliqarayev835@gmail.com>
2024-09-07 21:06:03 +0400

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,9 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#503AE7",
+        primary_dark: "#14142B",
+        secondary: "#1AD993",
       },
     },
   },
